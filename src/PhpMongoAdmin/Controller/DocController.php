98562d15bb67f615889c1059abd923a57b62d74f
harrysun <sunguangjun@126.com>
2015-07-18 18:28:38 +0800

    [Feature]Refine the formatter and add create/delete action for document

--- src/PhpMongoAdmin/Controller/DocController.php
+++ src/PhpMongoAdmin/Controller/DocController.php
@@ -2,6 +2,7 @@
 namespace PhpMongoAdmin\Controller;
 
 use PhpMongoAdmin\Base\Controller;
+use PhpMongoAdmin\Base\Formatter;
 use PhpMongoAdmin\Exception\InvalidArgumentException;
 
 /**
@@ -37,4 +38,46 @@ class DocController  extends Controller {
         return ['total' => $total, 'page' => $page, 'per-page' => $pageSize, 'items' => $items];
     }
 
+    /**
+     * Create or update a mongo document
+     * @param string $db Database name
+     * @param string $collection Collection name
+     * @return array
+     * @throws InvalidArgumentException
+     */
+    public function createAction($db, $collection) {
+        if (empty($db) || empty($collection)) {
+            throw new InvalidArgumentException('Database or collection name is empty');
+        }
+
+        $doc = $this->getParam();
+        if (empty($doc)) {
+            throw new InvalidArgumentException('Document is empty');
+        }
+
+        $doc = Formatter::json2Document($doc);
+        $coll = $this->getCollection($db, $collection);
+        return $coll->save($doc);
+    }
+
+    /**
+     * Delete a document with _id field
+     * @param string $db
+     * @param string $collection
+     * @return array
+     * @throws InvalidArgumentException
+     */
+    public function deleteAction($db, $collection) {
+        if (empty($db) || empty($collection)) {
+            throw new InvalidArgumentException('Database or collection name is empty');
+        }
+
+        $id = $this->getParam('id');
+        $id = Formatter::str2MongoId($id);
+        if ($id === false) {
+            throw new InvalidArgumentException('Document id is wrong');
+        }
+
+        return $this->getCollection($db, $collection)->remove(['_id' => $id]);
+    }
 }
